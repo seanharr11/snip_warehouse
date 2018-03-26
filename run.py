0288055884998fd6cfb86c230060ea39ddc6f4c5
Sean Harrington <seanharr11@gmail.com>
2018-03-26 18:58:11 -0400

    user_id kwarg in upload() func in run.py"

--- run.py
+++ run.py
@@ -1,14 +1,26 @@
 import asyncio
-from snp_matcher import SnvLoader
-
+import os
+from snp_matcher import SnvLoader, SnvUploader
+from snp_matcher.schema import init_db
 loop = asyncio.get_event_loop()
+
+DB_NAME = "snvs_chr_1"
 
 
 async def main():
-    snp_loadr = SnvLoader("sqlite:///data/snps.sql")
+    input("Are you sure you want to re-init DB?"
+          "This takes 2-3 hrs per chromosome")
+    init_db(DB_NAME)
+    snp_loadr = SnvLoader(DB_NAME)
     chr_suffixes = [i for i in range(1, 24)]
     chr_suffixes += ["X", "Y", "MT"]
-    for chr_suffix in chr_suffixes:
+    for chr_suffix in chr_suffixes[:3]:
         snp_loadr.download_dbsnp_file(f"refsnp-chr{chr_suffix}.json.gz")
-        snp_loadr.load_ref_snps(f"refsnp-chr{chr_suffix}.json")
+        await snp_loadr.load_ref_snps(f"refsnp-chr{chr_suffix}.json")
+        os.system(f"rm refsnp-chr{chr_suffix}.json")
+    snv_uploader = SnvUploader(DB_NAME)
+    await snv_uploader.connect()
+    with open("data/23_and_me_snps.txt", "r") as fp:
+        await snv_uploader.upload(fp, user_id=1)
+
 loop.run_until_complete(main())
